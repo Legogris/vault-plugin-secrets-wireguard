18ffb81481d29a7f9c01096d6242e7ba4cf585d4
Robert Edström <github@legogris.se>
2020-07-21 16:35:17 +0900

    only build for make bin

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,54 +19,54 @@ echo "--> Scratch dir"
 echo "    Creating"
 SCRATCH="$DIR/tmp"
 mkdir -p "$SCRATCH/plugins"
-
+echo "$SCRATCH"
 echo "--> Vault server"
 echo "    Writing config"
 tee "$SCRATCH/vault.hcl" > /dev/null <<EOF
 plugin_directory = "$SCRATCH/plugins"
 EOF
 
-echo "    Envvars"
-export VAULT_DEV_ROOT_TOKEN_ID="root"
-export VAULT_ADDR="http://127.0.0.1:8200"
+# echo "    Envvars"
+# export VAULT_DEV_ROOT_TOKEN_ID="root"
+# export VAULT_ADDR="http://127.0.0.1:8200"
 
 echo "    Starting"
-vault server \
-  -dev \
-  -log-level="debug" \
-  -config="$SCRATCH/vault.hcl" \
-  -dev-ha -dev-transactional -dev-root-token-id=root \
-  &
-sleep 2
-VAULT_PID=$!
+# vault server \
+#   -dev \
+#   -log-level="debug" \
+#   -config="$SCRATCH/vault.hcl" \
+#   -dev-ha -dev-transactional -dev-root-token-id=root \
+#   &
+# sleep 2
+# VAULT_PID=$!
 
 function cleanup {
   echo ""
   echo "==> Cleaning up"
-  kill -INT "$VAULT_PID"
-  rm -rf "$SCRATCH"
+  # kill -INT "$VAULT_PID"
+  # rm -rf "$SCRATCH"
 }
 trap cleanup EXIT
 
-echo "    Authing"
-vault login root &>/dev/null
+# echo "    Authing"
+# vault login root &>/dev/null
 
 echo "--> Building"
 go build -o "$SCRATCH/plugins/$PLUGIN_NAME" "./cmd/$PLUGIN_NAME" 
 SHASUM=$(shasum -a 256 "$SCRATCH/plugins/$PLUGIN_NAME" | cut -d " " -f1)
 
-echo "    Registering plugin"
-vault write sys/plugins/catalog/$PLUGIN_CATALOG_NAME \
-  sha_256="$SHASUM" \
-  command="$PLUGIN_NAME"
+# echo "    Registering plugin"
+# vault write sys/plugins/catalog/$PLUGIN_CATALOG_NAME \
+#   sha_256="$SHASUM" \
+#   command="$PLUGIN_NAME"
 
-echo "    Mounting plugin"
-vault secrets enable wireguard
+# echo "    Mounting plugin"
+# vault secrets enable wireguard
 
-if [ -e scripts/custom.sh ]
-then
-  . scripts/custom.sh
-fi
+# if [ -e scripts/custom.sh ]
+# then
+#   . scripts/custom.sh
+# fi
 
 echo "==> Ready!"
 wait $!
